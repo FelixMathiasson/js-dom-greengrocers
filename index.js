c03046325ba3826a9f4713088790a62c33cc1279
Felix <felix.mathiasson99@gmail.com>
2024-09-24 11:22:18 +0200

    can now add and remove items to cart and price updates

--- index.js
+++ index.js
@@ -70,6 +70,7 @@ function RenderBobsStore() {
     div.appendChild(image)
 
     const btn = document.createElement('button')
+    btn.onclick = () => AddItemToCart(item.id)
     btn.textContent = 'Add to cart'
     
     li.appendChild(div)
@@ -77,6 +78,78 @@ function RenderBobsStore() {
 
     storeItems.appendChild(li)
   })
+}
+
+function AddItemToCart(id) {
+  const item =  state.items.find(i => i.id === id)
+  const cItem = state.cart.find(c => c.id === id)
+  if(!cItem) {
+    state.cart.push({...item, quantity: 1})
+  } else {
+    cItem.quantity++
+  }
+  RenderCart()
+}
+
+function RenderCart() {
+  const container = document.querySelector('.cart--item-list')
+  container.innerHTML = ''
+
+  state.cart.forEach(cItem => {
+    const li = document.createElement('li')
+
+    const image = document.createElement('img')
+    image.alt = cItem.name;
+    image.className = 'cart--item-icon';
+    image.src = `assets/icons/${cItem.id}.svg`;
+    
+    const p = document.createElement('p')
+    p.textContent = cItem.name
+
+    const quantity = document.createElement('span')
+    quantity.className = 'quantity-text center'
+    quantity.textContent = cItem.quantity
+
+    const incBtn = document.createElement('button')
+    incBtn.className = 'quantity-btn add-btn center'
+    incBtn.textContent = '+'
+    incBtn.onclick = () => QuantityIncrease(cItem.id)
+
+    const decBtn = document.createElement('button')
+    decBtn.className = 'quantity-btn remove-btn center'
+    decBtn.textContent = '-'
+    decBtn.onclick = () => QuantityDecrease(cItem.id)
+
+    li.appendChild(image)
+    li.appendChild(p)
+    li.appendChild(quantity)
+    li.appendChild(incBtn)
+    li.append(decBtn)
+    
+
+    container.appendChild(li)
+  })
+  TotalCartUpdate()
+};
+
+function QuantityIncrease(id) {
+  const cItem = state.cart.find(i => i.id === id)
+  cItem.quantity++
+  RenderCart()
+}
+
+function QuantityDecrease(id) {
+  const cItem = state.cart.find(i => i.id === id)
+  cItem.quantity--
+  if(cItem.quantity === 0) {
+    state.cart = state.cart.filter(c => c.id !== id);
+  }
+  RenderCart()
+}
+
+function TotalCartUpdate() {
+  const amount = state.cart.reduce((sum, cItem) => sum + cItem.price * cItem.quantity, 0)
+  document.querySelector('.total-number').innerText  = '£' + amount.toFixed(2)
 }
 
 RenderBobsStore()
